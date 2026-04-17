a9144274e641bd1eb67bcdf71a0a8ece7963ee5d
Lorenaagaj <lorena.agaj@student.uni-pr.edu>
2026-04-17 19:06:12 +0200

    kerkimi i nje file

--- filemanage.c
+++ filemanage.c
@@ -46,3 +46,26 @@ void cmd_delete(const char *filename, char *output, size_t outsz){
     elsestrncpy(output, "[gabim] nuk u fshi dot file.", outsz - 1);
     output[outsz - 1] = '\0';
 }
+
+void cmd_search(const char *keyword, char *output, size_t outsz){
+    DIR *d = opendir("server_files");
+    if(!d){
+        strncpy(output,  "[gabim] server_files nuk u gjet.", outsz - 1);
+        output[outsz - 1] = '\0';
+        return;
+    }
+
+    struct dirent *entry;
+    output[0] = '\0';
+    while ((entry = readdir(d)) !=NULL){
+        if (entry->d_name[0] == '.') continue;
+        if(strst(entry->d_name, keyword)) {
+            strncat(output, entry->d_name, outsz - strlen(output) - 2);
+            strncat(output, "\n", outsz - strlen(output) - 1);
+        }
+    }
+
+    closedir(d);
+    if (output[0] == '\0') strncpy(output, "(nuk u gjeten rezultate)", outsz - 1);
+    output[outsz - 1] = '\0';
+}
